645ff1b47172764ebb15387c19bca12c3553610d
chaitanya-2498 <chaitan183@gmail.com>
2025-12-31 21:15:47 +0530

    Create revision.py

---
+++ revision.py
@@ -0,0 +1,39 @@
+#python basics 
+n=input("enter a no")
+s=input("enter a string")
+#string functions
+print("length  of your string is ",len(s))  #to calculate lenght of the string 
+#learn some more of them 
+#loops
+for a in range(0,len(s)):
+  print(s[a])
+while(a<n):
+#conditional statements
+if(s=="chaitanya"):
+  print("good")
+elif(s!="chaitanya"):
+  print("good")
+#functions 
+def sum(a,b):
+  return a+b
+
+#lists,tuples,sets,dictionaries
+#lists are mutable and have all unique values 
+#tuples are immutable and may have repeted values 
+#sets are also similar but a small diffrence 
+#dictionaries have a key value pair 
+list=[]
+tuple=()
+set={}
+dictionary={"key":"value"}
+#classes and objects 
+class student:
+  #constructor
+  def __init__(self):
+    
+object1=student(self)
+
+
+
+
+
